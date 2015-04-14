5226efd0d6630848a81ee1a79dc3cf0962d62685
sabachanna <shabir.saba@gmail.com>
2015-04-14 11:37:54 +0500

    Full refresh

---
+++ Chatt Main/Chat/Databse.swift
@@ -0,0 +1,13 @@
+//
+//  Databse.swift
+//  Chat
+//
+//  Created by Cloudkibo on 14/04/2015.
+//  Copyright (c) 2015 MyAppTemplates. All rights reserved.
+//
+
+import Foundation
+
+//import Squeal
+
+
---
+++ Chatt Main/Chat/RegisterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterViewController.swift
+//  Chat
+//
+//  Created by Cloudkibo on 24/03/2015.
+//  Copyright (c) 2015 MyAppTemplates. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+class RegisterViewController: UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet weak var firstNameTxt: UITextField!
+    @IBOutlet weak var lastNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var phoneTxt: UITextField!
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBAction func registerBtn(sender: AnyObject) {
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
