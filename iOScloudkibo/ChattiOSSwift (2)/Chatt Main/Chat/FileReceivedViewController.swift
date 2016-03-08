e9031278a119603cbaff00b8338d5b4c795a3341
sumairasaeed <sumaira.syedsaeed@gmail.com>
2016-03-08 08:04:39 +0500

    save file on icloud in progress. encountered an error

--- iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/FileReceivedViewController.swift
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/FileReceivedViewController.swift
@@ -32,26 +32,43 @@ class FileReceivedViewController: UIViewController,UIDocumentInteractionControll
         let docsDir1 = dirPaths[0]
         var documentDir=docsDir1 as NSString
         var filePathImage2=documentDir.stringByAppendingPathComponent(filejustreceivedname!)
-        
+        print("filepath2 is\(filePathImage2)")
         fileURL=NSURL(fileURLWithPath: filePathImage2)
-        var fileManager=NSFileManager.defaultManager()
-        var e:NSError!
-        do
-        {
-            print("saving to iCloud")
-            var dest=fileManager.URLForUbiquityContainerIdentifier(nil)
-            dest!.URLByAppendingPathComponent("cloudkiboDirectory")
-            var ans=try fileManager.setUbiquitous(true, itemAtURL: fileURL, destinationURL: dest!)
-            print("\(ans)")
-        }catch
-        {
-            print(error)
-        }
-        
+        var furl2=NSURL(fileURLWithPath: filePathImage2)
+        print("local furl2 is\(furl2)")
         
         documentInteractionController = UIDocumentInteractionController(URL: fileURL)
         documentInteractionController.delegate=self
-        documentInteractionController.presentOpenInMenuFromRect(CGRect(x: 20, y: 100, width: 300, height: 200), inView: self.view, animated: true)
+        //documentInteractionController.presentOpenInMenuFromRect(CGRect(x: 20, y: 100, width: 300, height: 200), inView: self.view, animated: true)
+        
+        
+        
+        
+        var fileManager=NSFileManager.defaultManager()
+        var e:NSError!
+                    print("saving to iCloud")
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), { () -> Void in
+                var dest=fileManager.URLForUbiquityContainerIdentifier(nil)
+                
+                do
+                {
+                    //var newdest=dest!.URLByAppendingPathComponent("Documents", isDirectory: true)
+                    //print("newdest is \(newdest.debugDescription)")
+                    //var ans=try fileManager.setUbiquitous(true, itemAtURL: self.fileURL, destinationURL: newdest)
+                    var ans=try fileManager.setUbiquitous(true, itemAtURL: furl2, destinationURL: dest!)
+                    
+                    print("ans is \(ans)")
+
+                }catch
+                {
+                    print("error is \(error)")
+                }
+                
+                
+                
+            })
+            
+        
     }
     
     @IBAction func btnFilePreviewPressed(sender: AnyObject) {
@@ -82,6 +99,56 @@ class FileReceivedViewController: UIViewController,UIDocumentInteractionControll
         
         documentInteractionController=nil
     }
+    
+    
+    
+    func moveToiCloud(filetosave:NSURL)
+    {
+        var sourceURL:NSURL=filetosave
+        //NSString destFileName
+    }
+    
+    
+    /*
+    (void)moveFileToiCloud:(FileRepresentation *)fileToMove {
+    NSURL *sourceURL = fileToMove.url;
+    NSString *destinationFileName = fileToMove.fileName;
+    NSURL *destinationURL = [self.documentsDir URLByAppendingPathComponent:destinationFileName];
+    
+    dispatch_queue_t q_default;
+    q_default = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(q_default, ^(void) {
+    NSFileManager *fileManager = [[[NSFileManager alloc] init] autorelease];
+    NSError *error = nil;
+    BOOL success = [fileManager setUbiquitous:YES itemAtURL:sourceURL
+    destinationURL:destinationURL error:&error];
+    dispatch_queue_t q_main = dispatch_get_main_queue();
+    dispatch_async(q_main, ^(void) {
+    if (success) {
+    FileRepresentation *fileRepresentation = [[FileRepresentation alloc]
+    initWithFileName:fileToMove.fileName url:destinationURL];
+    [_fileList removeObject:fileToMove];
+    [_fileList addObject:fileRepresentation];
+    NSLog(@"moved file to cloud: %@", fileRepresentation);
+    }
+    if (!success) {
+    NSLog(@"Couldn't move file to iCloud: %@", fileToMove);
+    }
+    });
+    });
+    }
+    
+    */
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
