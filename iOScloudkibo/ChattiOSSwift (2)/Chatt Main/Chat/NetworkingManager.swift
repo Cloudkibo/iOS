713ae72b04680ec97b941f5381d9dec3990de1a9
sumairasaeed <sumaira.syedsaeed@gmail.com>
2016-08-05 17:49:12 +0500

    work on file upload

--- iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/NetworkingManager.swift
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/NetworkingManager.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import Alamofire
-
+import SwiftyJSON
 class NetworkingManager
 {
     static let sharedManager = NetworkingManager()
@@ -19,19 +19,47 @@ class NetworkingManager
         return Alamofire.Manager(configuration: NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier(bundleIdentifier + ".background"))
     }()
     
-    func uploadFile()
+    func uploadFile(filePath:String)
     {
+        var parameterJSON = JSON([
+            "id_user": "test"
+            ])
+        // JSON stringify
+        let parameterString = parameterJSON.rawString(NSUTF8StringEncoding, options: NSJSONWritingOptions.PrettyPrinted )
+        let jsonParameterData = parameterString!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        var imageData=NSData(contentsOfFile: filePath)
+        
     Alamofire.upload(
     .POST,
     "http://api.imagga.com/v1/content",
     headers: ["Authorization" : "Basic xxx"],
     multipartFormData: { multipartFormData in
-    multipartFormData.appendBodyPart(data: imageData, name: "imagefile",
+    multipartFormData.appendBodyPart(data: imageData!, name: "imagefile",
     fileName: "image.jpg", mimeType: "image/jpeg")
+    multipartFormData.appendBodyPart(data: jsonParameterData!, name: "goesIntoForm")
+        
     },
     encodingCompletion: { encodingResult in
+        switch encodingResult {
+        case .Success(let upload, _, _):
+            upload.progress { bytesWritten, totalBytesWritten, totalBytesExpectedToWrite in
+                dispatch_async(dispatch_get_main_queue()) {
+                    let percent = (Float(totalBytesWritten) / Float(totalBytesExpectedToWrite))
+                    /////progress(percent: percent)
+                    print("percentage is \(percent)")
+                }
+            }
+            upload.validate()
+            upload.responseJSON { response in
+            }
+        case .Failure(let encodingError):
+            print(encodingError)
+        }
+        
     }
     )
+        
+        
     }
     
     
