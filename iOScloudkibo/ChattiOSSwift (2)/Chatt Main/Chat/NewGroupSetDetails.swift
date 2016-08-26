8f7f7b75d915cb8e47ec92c0121045dc42bfe822
sumairasaeed <sumaira.syedsaeed@gmail.com>
2016-08-27 04:52:13 +0500

    work on group messaginf UI sreens

---
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/NewGroupSetDetails.swift
@@ -0,0 +1,13 @@
+//
+//  NewGroupSetDetails.swift
+//  kiboApp
+//
+//  Created by Cloudkibo on 27/08/2016.
+//  Copyright © 2016 MyAppTemplates. All rights reserved.
+//
+
+import Cocoa
+
+class NewGroupSetDetails: UITableViewController {
+
+}
