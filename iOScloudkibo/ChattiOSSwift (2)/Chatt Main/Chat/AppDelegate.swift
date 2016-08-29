a5a0c1f2822a4a341b84c9ecd3599c5553f94dc1
sumairasaeed <sumaira.syedsaeed@gmail.com>
2016-08-29 05:51:26 +0500

    correcting app crash error

--- iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/AppDelegate.swift
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/AppDelegate.swift
@@ -126,7 +126,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,AppDelegateScreenDelegate 
         UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false);
         
         //RESET TEMP
- KeychainWrapper.removeObjectForKey("username")
+/// KeychainWrapper.removeObjectForKey("username")
         
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         //var socketObj=LoginAPI(url:"\(Constants.MainUrl)")
