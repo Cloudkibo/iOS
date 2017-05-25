6dcaeee98acaac8e9e254324a356053d367222d4
sumairasaeed <sumaira.syedsaeed@gmail.com>
2017-05-25 18:53:19 +0500

    work on 634

--- iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Pods/Target Support Files/Pods-Chat/Pods-Chat-frameworks.sh
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Pods/Target Support Files/Pods-Chat/Pods-Chat-frameworks.sh
@@ -91,10 +91,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ActiveLabel/ActiveLabel.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyCam/SwiftyCam.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ActiveLabel/ActiveLabel.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyCam/SwiftyCam.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
