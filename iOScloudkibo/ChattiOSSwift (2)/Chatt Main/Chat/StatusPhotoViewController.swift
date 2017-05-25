67e72177c169e8e9531f3be2fa30b27c98a42d3f
sumairasaeed <sumaira.syedsaeed@gmail.com>
2017-05-26 00:56:43 +0500

    work on 633

---
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/StatusPhotoViewController.swift
@@ -0,0 +1,74 @@
+//
+//  StatusPhotoViewController.swift
+//  kiboApp
+//
+//  Created by Cloudkibo on 26/05/2017.
+//  Copyright © 2017 MyAppTemplates. All rights reserved.
+//
+
+import UIKit
+
+/*Copyright (c) 2016, Andrew Walz.
+ 
+ Redistribution and use in source and binary forms, with or without modification,are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+import UIKit
+
+class StatusPhotoViewController: UIViewController,UIImagePickerControllerDelegate {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    private var backgroundImage: UIImage
+    
+    init(image: UIImage) {
+        self.backgroundImage = image
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray
+        let backgroundImageView = UIImageView(frame: view.frame)
+        backgroundImageView.contentMode = UIViewContentMode.scaleAspectFit
+        backgroundImageView.image = backgroundImage
+        view.addSubview(backgroundImageView)
+        let cancelButton = UIButton(frame: CGRect(x: 10.0, y: 10.0, width: 30.0, height: 30.0))
+        cancelButton.setImage(#imageLiteral(resourceName: "cancel"), for: UIControlState())
+        cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        view.addSubview(cancelButton)
+    }
+    
+    func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+//}
