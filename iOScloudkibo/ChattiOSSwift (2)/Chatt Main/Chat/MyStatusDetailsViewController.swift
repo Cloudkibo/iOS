c79ab25885ea8998c192ad80b318450a922a2c8b
sumairasaeed <sumaira.syedsaeed@gmail.com>
2017-05-25 18:36:15 +0500

    work on 634

--- iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/MyStatusDetailsViewController.swift
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/MyStatusDetailsViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
-
+import ActiveLabel
 class MyStatusDetailsViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     
     var messages:NSMutableArray!
     
+    @IBOutlet weak var btnEditOutlet: UIBarButtonItem!
     @IBOutlet weak var tblMyStatus: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,29 +22,160 @@ class MyStatusDetailsViewController: UIViewController,UITableViewDataSource,UITa
         messages.add(["picID":"1","time":"1 hour ago","viewCount":"2"])
         messages.add(["picID":"2","time":"just now","viewCount":"0"])
         
+        tblMyStatus.allowsMultipleSelectionDuringEditing = true
+        tblMyStatus.setEditing(true, animated: false)
+        
         // Do any additional setup after loading the view.
     }
     func numberOfSections(in tableView: UITableView) -> Int {
+
         return 2
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if(section==0)
         {
-            return 1
+            return messages.count
         }
         else{
-            if(messages.count>0)
+            
+                return 0
+            }
+        
+    }
+    
+    
+    @IBAction func btnEditPressed(_ sender: Any) {
+        
+        
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var messageDic = messages.object(at: indexPath.row) as! [String : String];
+        // NSLog(messageDic["message"]!, 1)
+        let viewCount = messageDic["viewCount"] as NSString!
+        let timeDict = messageDic["time"] as NSString!
+        
+        var cell = tblMyStatus.dequeueReusableCell(withIdentifier: "MyStatusUpdatesCell")! as! UITableViewCell
+        
+        if(indexPath.section==0)
+        {
+            cell = tblMyStatus.dequeueReusableCell(withIdentifier: "MyStatusUpdatesCell")! as! UITableViewCell
+            
+        }
+            /*
+        else{
+            if(messages.count<1)
             {
-                return messages.count
+                cell = tblStatusUpdates.dequeueReusableCell(withIdentifier: "StatusCell")! as! UITableViewCell
             }
             else{
-                return 1
+                cell = tblStatusUpdates.dequeueReusableCell(withIdentifier: "myStatusCell")! as! UITableViewCell
+                var name=cell.viewWithTag(2) as! UILabel
+                var time=cell.viewWithTag(3) as! UILabel
+                name.text=nameDict as String?
+                time.text=timeDict as String?
+                
+            }
+            
+        }
+        */
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 71
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        if(section==0)
+        {
+            return 25
+        }
+        else{
+            return tableView.frame.height
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if(section==1)
+        {
+    
+            var outerview=ActiveLabel.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.size.width, height: tableView.frame.height))
+            
+            var headerview=ActiveLabel.init(frame: CGRect.init(x: 10, y: 5, width: tableView.frame.size.width-10, height: 60))
+        headerview.text="Updates will be removed from all the contacts after 24hours"
+        headerview.lineBreakMode = .byWordWrapping
+        headerview.numberOfLines=0
+            headerview.textAlignment = .center
+            outerview.addSubview(headerview)
+       // outerview.backgroundColor = UIColor.lightGray
+        return outerview
+        }
+        else{
+                var headerview=ActiveLabel.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.size.width, height: 30))
+           // headerview.backgroundColor = UIColor.lightGray
+            return headerview
+        }
+    }
+   /*
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if(section==1)
+        {
+            return "Updates will be removed after 24hours"
+        }
+        else{
+            return ""
+        }
+    }*/
+
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        var messageDic = messages.object(at: indexPath.row) as! [String : AnyObject];
+        
+        //// if editingStyle == .Delete {
+        if(editingStyle == UITableViewCellEditingStyle.delete){
+            
+            var messageDic = messages.object(at: indexPath.row) as! [String : String];
+            var listname=messageDic["listname"] as! NSString
+            var uniqueid=messageDic["uniqueid"] as! NSString
+            
+            var membersnames=messageDic["membersnames"] as! NSString
+            
+            //!!sqliteDB.deleteBroadcastList(uniqueid as String)
+            //!!retrieveBroadCastLists()
+           
+            tblMyStatus.reloadData()
+            
+            
+            //delete
+            
+        }
+        else{
+            if(editingStyle == UITableViewCellEditingStyle.insert)
+            {
+                print("another swipe button")
             }
         }
     }
-
-
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tblMyStatus.setEditing(editing, animated: animated)
+        print("editingggg....2")
+        
+        tblMyStatus.reloadData()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
