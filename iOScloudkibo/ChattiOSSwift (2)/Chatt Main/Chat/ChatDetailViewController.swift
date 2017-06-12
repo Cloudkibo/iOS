d99aedb1c5dd25672a7b25d1051e141b419aae1e
sumairasaeed <sumaira.syedsaeed@gmail.com>
2017-06-13 00:05:36 +0500

    work on 625

--- iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/ChatDetailViewController.swift
+++ iOScloudkibo/ChattiOSSwift (2)/Chatt Main/Chat/ChatDetailViewController.swift
@@ -5560,8 +5560,32 @@ var isKiboContact="false"
                 tblForChats.dequeueReusableCell(withIdentifier: "ChatDayStatusSentCell")! as UITableViewCell
             
             //=====cell.tag = indexPath.row
+            let stackview = cell.viewWithTag(2) as! UIStackView
+            let textLable = cell.viewWithTag(3) as! UILabel
+            let timeLabel = cell.viewWithTag(4) as! UILabel
             
-           
+            let viewStackview1 = stackview.viewWithTag(1)
+           // let statusImage = stackview.viewWithTag(2) as! UIImageView
+            
+            let viewStackview11 = viewStackview1?.viewWithTag(1)
+             let viewStackview1_statuslabel = viewStackview11?.viewWithTag(1) 
+            let viewStackview1_imgCamera = viewStackview11?.viewWithTag(2) as! UIImage
+            let viewStackview1_img_video_label = viewStackview11?.viewWithTag(3) as! UILabel
+
+           textLable.text=msg! as! String
+            
+            let formatter = DateFormatter();
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+            //formatter.dateFormat = "MM/dd hh:mm a";
+            formatter.timeZone = TimeZone.autoupdatingCurrent
+            print("line 2055")
+            let defaultTimeZoneStr = formatter.date(from: date2 as! String)
+            //print("defaultTimeZoneStr \(defaultTimeZoneStr)")
+            
+            let formatter2 = DateFormatter();
+            formatter2.timeZone=TimeZone.autoupdatingCurrent
+            formatter2.dateFormat = "MM/dd hh:mm a";
+            let displaydate=formatter2.string(from: defaultTimeZoneStr!)
             
         }
 
